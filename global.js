357eb05718e98d185c22e15cbe33af3551650595
Derek George <derek68038@gmail.com>
2015-03-09 16:17:19 -0500

    Done with this project. You have to click on all three tabs then their bottom borders show correctly. Not sure why.

--- global.js
+++ global.js
@@ -1,18 +1,45 @@
 window.onload = function() {
-  aboutTab = document.getElementById("about");
-  concertsTab = document.getElementById("concerts");
-  discTab = document.getElementById("discography");
+  var aboutTab = document.getElementById("about");
+  var aboutTabAnchor = document.getElementsByTagName("a")[0];
+  var aboutTabList = document.getElementsByTagName("li")[0];
   
-  aboutTab.addEventListener("click", goToTab);
-  concertsTab.addEventListener("click", goToTab);
-  discTab.addEventListener("click", goToTab);
+  var concertsTab = document.getElementById("concerts");
+  var concertsTabAnchor = document.getElementsByTagName("a")[1];
+  var concertsTabList = document.getElementsByTagName("li")[1]; 
   
-  aboutTab.click = function() {return false};
-  concertsTab.click = function() {return false};
-  discTab.click = function() {return false};  
-}
+  var discTab = document.getElementById("discography");
+  var discTabAnchor = document.getElementsByTagName("a")[2];
+  var discTabList = document.getElementsByTagName("li")[2];
+  
+  
+  aboutTabAnchor.addEventListener("click", function() {
+    aboutTabList.style.backgroundColor="gray";
+    aboutTabList.style.borderBottom="gray";
+    aboutTab.style.display="block";
+    concertsTabList.style.backgroundColor="firebrick";
+    concertsTab.style.display="none";
+    discTabList.style.backgroundColor="firebrick";
+    discTab.style.display="none";
+  });
+  
+  concertsTabAnchor.addEventListener("click", function() {
+    concertsTabList.style.backgroundColor="gray";
+    concertsTabList.style.borderBottom="gray";
+    concertsTab.style.display="block";
+    aboutTabList.style.backgroundColor="firebrick";
+    aboutTab.style.display="none";
+    discTabList.style.backgroundColor="firebrick";
+    discTab.style.display="none";
+  }); 
+  
+  discTabAnchor.addEventListener("click", function() {
+    discTabList.style.backgroundColor="gray";
+    discTabList.style.borderBottom="gray";
+    discTab.style.display="block";
+    aboutTabList.style.backgroundColor="firebrick";
+    aboutTab.style.display="none";
+    concertsTabList.style.backgroundColor="firebrick";
+    concertsTab.style.display="none";
+  });     
+};
 
-function goToTab () {
-  
-  
-}
